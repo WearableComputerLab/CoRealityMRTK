9759b2863ddea184d01cc7223b269d8f30a2387c
jemmec <jeremy.mcdade@unisa.edu.au>
2022-01-11 12:19:22 +1030

    Made user name look at and overlay

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    void Update()
+    {
+        transform.LookAt(transform.position - Camera.main.transform.position);
+    }
+}
